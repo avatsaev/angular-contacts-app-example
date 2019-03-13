c41e2460f6bfc1117475ae1ba031b79abf75d616
Vatsaev Aslan <a.vatsaev@gmail.com>
2019-03-13 15:48:20 +0100

    fix: fix creation bug

--- src/app/views/contacts/contact-new/contact-new.component.ts
+++ src/app/views/contacts/contact-new/contact-new.component.ts
@@ -1,10 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
 import { Contact } from '@app/core/models';
-import { Subscription} from 'rxjs';
 import { Router} from '@angular/router';
 import { ContactsStoreFacade} from '@app/contacts-store/contacts-store.facade';
-import {ContactsEffects} from '@app/contacts-store/contacts-effects';
-import {CreateSuccess} from '@app/contacts-store/contacts-actions';
 
 @Component({
   selector: 'app-contact-new',
@@ -14,28 +11,20 @@ import {CreateSuccess} from '@app/contacts-store/contacts-actions';
 })
 export class ContactNewComponent implements OnInit, OnDestroy {
 
-  redirectSub: Subscription;
-
   constructor(
     private contactsFacade: ContactsStoreFacade,
-    private router: Router,
-    private contactsEffects: ContactsEffects
+    private router: Router
   ) { }
 
   ngOnInit() {
-
-    this.redirectSub = this.contactsEffects.create$.subscribe(
-      (action: CreateSuccess) => this.router.navigate(['/contacts', action.payload.id])
-    );
-
   }
 
   ngOnDestroy() {
-    this.redirectSub.unsubscribe();
   }
 
   submitted(contact: Contact) {
     this.contactsFacade.createContact(contact);
+    this.router.navigate(['/contacts']);
   }
 
 }
