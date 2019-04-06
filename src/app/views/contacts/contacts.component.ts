acb15290d739903655f2e14a845bd8ce95f881e1
Marina Pavlova <marina-fam@yandex.ru>
2019-04-06 23:13:20 +0300

    Add mobile support

--- src/app/views/contacts/contacts.component.ts
+++ src/app/views/contacts/contacts.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
-  host: { 'class': 'f-grow-1' },
+  host: { 'class': 'f-grow-1 scroll-content' },
   template: `<router-outlet></router-outlet>`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
