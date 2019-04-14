ef45001747e3a8f8adcb47e45a0bdecf25a6385d
Vatsaev Aslan <a.vatsaev@gmail.com>
2019-04-14 21:11:03 +0200

    fix: toolbar

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import {filter, map, shareReplay} from 'rxjs/operators';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,18 +10,31 @@ import { Subscription } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToolbarComponent implements OnInit, OnDestroy {
-  pageUrl: string;
+
+  pageUrl$: Observable<string> = this.router.events.pipe(
+    filter(event => event instanceof NavigationStart),
+    map((val: NavigationStart) => val.url),
+    shareReplay(1)
+  );
+
+  isShowHome$ = this.pageUrl$.pipe(
+    map(pageUrl => pageUrl !== '/contacts')
+  );
+
+
+  isShowAdd$ = this.pageUrl$.pipe(
+    map(pageUrl => pageUrl !== '/contacts/new')
+  );
+
   private subscriptions: Subscription = new Subscription();
 
   constructor(private router: Router) {
-    const routerSubscr = router.events.pipe(filter(event => event instanceof NavigationStart))
-      .subscribe((val: NavigationStart) => this.pageUrl = val.url);
-    this.subscriptions.add(routerSubscr);
+
   }
 
   ngOnInit() {
-
   }
+
   public ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
